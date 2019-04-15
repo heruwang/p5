0cf8ed47eeb2e9bcc5741c8f9c025a37a448410f
Heru Wang <heru.wang@sjsu.edu>
2019-04-14 22:39:07 -0700

    ad

--- adinteraction/sketch.js
+++ adinteraction/sketch.js
@@ -1,19 +1,27 @@
 let ad = [];
 let i = 0;
 
-function setup(){
-  ad[0] = document.querySelector("#ad0");
-  ad[0] = document.querySelector("#ad0");
-  ad[0] = document.querySelector("#ad0");
-  ad[0] = document.querySelector("#ad0");
-  setInterval(changeImage, 1000);
+function setup() {
+
+    ad[0] = document.getElementById("ad0");
+    ad[1] = document.getElementById("ad1");
+    ad[2] = document.getElementById("ad2");
+    ad[3] = document.getElementById("ad3");
+    ad[4] = document.getElementById("ad4");
+    ad[5] = document.getElementById("ad5");
+    ad[6] = document.getElementById("ad6");
+    ad[7] = document.getElementById("ad7");
+    ad[8] = document.getElementById("ad8");
+
 }
 
 function changeImage() {
-  if (i<ad.length)
-  {
-    ad[i].style.visibility = visible;
+  if (i < ad.length) {
+    ad[i].style.visibility = "visible";
     i++;
   }
+}
 
+function clickFunc() {
+  setInterval(changeImage, 1500);
 }
